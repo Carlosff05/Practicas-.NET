284d2eeac71f1cef3a9b981a44f82c8885c8bb1f
Carlos Fernández <carlos.ffernandez@cojali.com>
2026-03-24 16:19:24 +0100

    Ejemplo con copia de matrices

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,16 +4,40 @@
     {
         public static void Main(string[] args)
         {
+            //Declaro la primera matriz
             string[][] nombres = new string[3][];
             nombres[0] = new string[] { "Juan", "Pepe" };
             nombres[1] = new string[] { "Eva", "Ana", "Julia", "Sandy"};
             nombres[2] = new string[] { "Maite" };
             
+            //Imprimo la primera matriz
+            Console.WriteLine("PRIMERA MATRIZ");
             for (int i = 0; i < nombres.Length; i++)
             {
                 for (int j = 0; j < nombres[i].Length; j++)
                 {
                     Console.Write(nombres[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+            //Declaro la segunda matriz
+            string[][] nombresCopia = new string[nombres.Length][];
+            
+            //Copio la primera matriz en la segunda
+            for (int i = 0; i < nombresCopia.Length; i++)
+            {
+                nombresCopia[i] = new string[nombres[i].Length];
+                nombres[i].CopyTo(nombresCopia[i], 0);
+            }
+            
+            //Imprimimos la segunda matriz, comprobando así que se ha copiado correctamente
+            Console.WriteLine("SEGUNDA MATRIZ");
+            for (int i = 0; i < nombresCopia.Length; i++)
+            {
+                for (int j = 0; j < nombresCopia[i].Length; j++)
+                {
+                    Console.Write(nombresCopia[i][j] + " ");
                 }
                 Console.WriteLine();
             }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb47c80ff8bef48fb8dba32a18a6b64156afc33")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
